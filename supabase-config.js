70f4720ea6641f1af47bade87cd0ea2c45104454
sirjappa-cell <sirjappa@gmail.com>
2026-03-18 22:16:37 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-window.ACRETA_SUPABASE_URL = "https://eeuajdzjlzmplkqglulz.supabase.co";
+window.ACRETA_SUPABASE_URL = "https://eeuajzdjlzmplkqglulz.supabase.co";
 window.ACRETA_SUPABASE_ANON_KEY = "sb_publishable_ssBueMD2SKidUCIDNjToAg_N2u2ED7Y";
